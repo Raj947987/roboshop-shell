ea2cec48f7b2e900c4bbedf664a647f80b9410e7
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-05-29 18:53:18 +0530

    roboshop nginx script making

--- mogodb.sh
+++ mogodb.sh
@@ -3,7 +3,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo   &>>/tmp/roboshop.log
 echo -e "\e[32mInstalling mongo db\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 echo -e "\e[33mEdit the network gateways\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[34menable the mongod service\e[0m"
 systemctl enable mongod  &>>/tmp/roboshop.log
 echo -e "\e[35mRestart the mongodb service\e[0m"
